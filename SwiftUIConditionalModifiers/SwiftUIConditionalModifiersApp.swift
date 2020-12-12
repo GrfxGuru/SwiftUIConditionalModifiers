1dba205cf5db014bea598a4406983c11a7bbe693
Peter Witham <peter.witham@gmail.com>
2020-12-12 03:13:51 -0600

    Initial commit
    Initial commit of the new project

---
+++ SwiftUIConditionalModifiers/SwiftUIConditionalModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIConditionalModifiersApp.swift
+//  SwiftUIConditionalModifiers
+//
+//  Created by Peter Witham on 12/12/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIConditionalModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
